4e46e4fdd0d75220fde979f87caa3a748326d9a1
ssayebee <ssayebee@gmail.com>
2019-12-16 02:16:11 +0900

    Add: login page, login request

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const getPosts = async () => {
-    const response = await axios.get('/posts');
+    const response = await axios.get('/api/articles');
     return response.data;
 };
 
 export const getPostById = async id => {
-    const response = await axios.get(`/posts/${id}`);
+    const response = await axios.get(`/api/articles/${id}`);
     return response.data;
 };
--- src/modules/pages.js
+++ src/modules/pages.js
@@ -1,13 +1,17 @@
 import { takeEvery, getContext } from 'redux-saga/effects'
 
+const GO_TO_SIGNIN = 'GO_TO_SIGNIN';
 const GO_TO_HOME = 'GO_TO_HOME';
 const GO_TO_USER = 'GO_TO_USER';
 const GO_TO_EXPLORE = 'GO_TO_EXPLORE';
 
-
+function* goToSigninSaga() {
+    const history = yield getContext('history');
+    history.push('/signin');
+}
 function* goToHomeSaga() {
     const history = yield getContext('history');
-    history.push('/');
+    history.push('/home');
 }
 function* goToUserSaga() {
     const history = yield getContext('history');
@@ -19,11 +23,13 @@ function* goToExploreSaga() {
 }
 
 export function* PageSaga(){
+    yield takeEvery(GO_TO_SIGNIN, goToSigninSaga);
     yield takeEvery(GO_TO_HOME, goToHomeSaga);
     yield takeEvery(GO_TO_USER, goToUserSaga);
     yield takeEvery(GO_TO_EXPLORE, goToExploreSaga);
 }
 
+export const goToSignin = () => ({ type: GO_TO_SIGNIN });
 export const goToHome = () => ({ type: GO_TO_HOME });
 export const goToUser = () => ({ type: GO_TO_USER });
 export const goToExplore = () => ({ type: GO_TO_EXPLORE});
